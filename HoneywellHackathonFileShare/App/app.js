e13faa92d3f385d21fa251338ee948db14560660
Praveen <praveenpk59@gmail.com>
2017-09-23 10:43:31 +0530

    Adding angular nuget package

--- HoneywellHackathonFileShare/App/app.js
+++ HoneywellHackathonFileShare/App/app.js
@@ -1,1 +1,4 @@
-﻿
+﻿var fileShareApp = angular.module('fileShareApp', ['ngRoute']);
+fileShareApp.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+
+}]);
