8d780f3946f9020eff476aac41dfc9c8c81ba914
Sunilchaurha@gmail.com <Sunilchaurha@gmail.com>
2017-09-23 12:00:10 +0530

    angular module

--- HoneywellHackathonFileShare/App/app.js
+++ HoneywellHackathonFileShare/App/app.js
@@ -1,4 +1,17 @@
 ﻿var fileShareApp = angular.module('fileShareApp', ['ngRoute']);
-fileShareApp.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
 
+
+fileShareApp.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+    $routeProvider
+    .when("/Home", {
+        templateUrl: "App/efHomePage.html",
+        controller:"efHomeController"
+    })
+    .otherwise({
+        redirectTo:"/Home"
+    });
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
 }]);
---
+++ HoneywellHackathonFileShare/App/efHomeController.js
@@ -0,0 +1,10 @@
+﻿fileShareApp.controller('efHomeController', ['$scope','$location', function ($scope, $location) {
+
+    $scope.efBrowseFile = "Click to add File";
+
+    $scope.Upload = function(){
+
+    };
+}
+
+]);
