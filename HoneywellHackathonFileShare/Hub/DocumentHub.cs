c7ad918eecc4464e2c2aaa39576dcbfe83172e08
Vishal Anand <vishal_24_anand@hotmail.com>
2017-09-23 12:25:14 +0530

    added signalR

---
+++ HoneywellHackathonFileShare/Hub/DocumentHub.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HoneywellHackathonFileShare.Hub
+{
+    public class DocumentHub : Microsoft.AspNet.SignalR.Hub
+    {
+        private Dictionary<string, string> _userMapping = new Dictionary<string, string>();
+
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        //public override Task OnConnected()
+        //{
+          
+        //}
+    }
+}
